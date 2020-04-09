111502d8d8eab4be487e6525d8f40557c9b69635
ybyusbashh <ybyusbashh@hotmail.co.uk>
2020-04-09 17:11:20 +0100

    challenge 1 build

--- src/estimator.js
+++ src/estimator.js
@@ -1,9 +1,18 @@
 const covid19ImpactEstimator = (data) => {
   const input = data;
+  const { reportedCases } = data;
+  //const currentlyInfected = reportedCases * 10;
+  const infectionsByRequestedTime = currentlyInfected * Math(2, 10);
   return {
     data: input,
-    impact: {},
-    severeImpact: {}
+    impact: {
+      currentlyInfected: reportedCases * 10,
+      infectionsByRequestedTime: currentlyInfected * Math(2, 10)
+    },
+    severeImpact: {
+      currentlyInfected: reportedCases * 50,
+      infectionsByRequestedTime: currentlyInfected * Math(2, 10)
+    }
   };
 };
 
