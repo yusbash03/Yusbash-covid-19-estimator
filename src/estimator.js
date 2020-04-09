0cf9cff295874eb01e326200a410510ad672d5c6
ybyusbashh <ybyusbashh@hotmail.co.uk>
2020-04-09 21:09:17 +0100

    period type changed to months

--- src/estimator.js
+++ src/estimator.js
@@ -7,7 +7,7 @@ const covid19ImpactEstimator = (data) => {
         avgDailyIncomeInUSD: 5,
         avgDailyIncomePopulation: 0.71
       },
-      periodType: 'days',
+      periodType: 'months',
       timeToElapse: 58,
       reportedCases: 674,
       population: 66622705,
