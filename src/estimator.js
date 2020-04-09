e8345d6843445d8e6171e718dbbc7e95feead0f9
ybyusbashh <ybyusbashh@hotmail.co.uk>
2020-04-09 21:04:48 +0100

    data added

--- src/estimator.js
+++ src/estimator.js
@@ -1,5 +1,19 @@
 const covid19ImpactEstimator = (data) => {
-  const input = data;
+  const input = {
+    data: {
+      region: {
+        name: 'Africa',
+        avgAge: 19.7,
+        avgDailyIncomeInUSD: 5,
+        avgDailyIncomePopulation: 0.71
+      },
+      periodType: 'days',
+      timeToElapse: 58,
+      reportedCases: 674,
+      population: 66622705,
+      totalHospitalBeds: 1380614
+    }
+  };
   const { reportedCases } = data;
 
   const imapactCurrentlyInfected = reportedCases * 10;
